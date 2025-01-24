e98f56002c130453fb2bba3a96f314c65ad7698e
Viktor Scharf <scharf.vi@gmail.com>
2025-01-24 15:35:42 +0100

    run all tests

--- tests/acceptance/run_all_tests.sh
+++ tests/acceptance/run_all_tests.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # LOCAL TEST WITHOUT EXTRA ENVS
-TEST_SERVER_URL="https://localhost:9200"
+TEST_SERVER_URL="https://opencloud-server:9200"
+OC_WRAPPER_URL="http://opencloud-server:5200"
 EXPECTED_FAILURES_FILE="tests/acceptance/expected-failures-localAPI-on-OCIS-storage.md"
 EXPECTED_FAILURES_FILE_FROM_CORE="tests/acceptance/expected-failures-API-on-OCIS-storage.md"
 
@@ -32,6 +33,8 @@ SUITES=(
     "apiSharingNgLinkSharePermission"
     "apiSharingNgLinkShareRoot"
     "apiAccountsHashDifficulty"
+    "apiSearchContent"
+    "apiNotification"
 )
 
 # List of suites from core
@@ -65,6 +68,27 @@ CORE_SUITES=(
     "coreApiWebdavUploadTUS"
 )
 
+EXTRA_SUITE=(
+    "apiAntivirus"
+    "apiOcm"
+    "apiCollaboration"
+    "apiAuthApp"
+    "cliCommands"
+)
+
+E2E_SUITES=(
+    "admin-settings"
+    "file-action"
+    "journeys"
+    "navigation"
+    "search"
+    "shares"
+    "spaces"
+    "user-settings"
+)
+
+EXTRA_E2E_SUITE="app-providerapp-store,keycloak,ocm,oidc"
+
 # Create log directory
 LOG_DIR="./suite-logs"
 mkdir -p "$LOG_DIR"
@@ -80,7 +104,7 @@ for SUITE in "${SUITES[@]}"; do
     LOG_FILE="$LOG_DIR/${SUITE}.log"
 
     # Run suite
-    make test-acceptance-api TEST_SERVER_URL=$TEST_SERVER_URL EXPECTED_FAILURES_FILE=$EXPECTED_FAILURES_FILE BEHAT_SUITE=$SUITE > "$LOG_FILE" 2>&1
+    make test-acceptance-api TEST_SERVER_URL=$TEST_SERVER_URL OC_WRAPPER_URL=$OC_WRAPPER_URL EXPECTED_FAILURES_FILE=$EXPECTED_FAILURES_FILE BEHAT_SUITE=$SUITE > "$LOG_FILE" 2>&1
     
     # Check if suite was successful
     if [ $? -eq 0 ]; then
@@ -101,6 +125,30 @@ for SUITE in "${CORE_SUITES[@]}"; do
 
     # Run suite
     make test-acceptance-api TEST_SERVER_URL=$TEST_SERVER_URL EXPECTED_FAILURES_FILE=$EXPECTED_FAILURES_FILE_FROM_CORE BEHAT_SUITE=$SUITE > "$LOG_FILE" 2>&1
+    
+    # Check if suite was successful
+    if [ $? -eq 0 ]; then
+        echo "✅ Suite $SUITE completed successfully."
+        ((SUCCESS_COUNT++))
+    else
+        echo "❌ Suite $SUITE failed. Check log: $LOG_FILE"
+        ((FAILURE_COUNT++))
+    fi
+done
+
+# Run e2e suites
+for SUITE in "${E2E_SUITES[@]}"; do
+    echo "=============================================="
+    echo "Running e2e suite: $SUITE"
+    echo "=============================================="
+
+    LOG_FILE="$LOG_DIR/${SUITE}.log"
+
+    # Run suite
+    (
+        cd services/web/_web/tests/e2e || exit 1
+        BASE_URL_OPEN_CLOUD=$TEST_SERVER_URL RETRY=1 HEADLESS=true ./run-e2e.sh --suites $SUITE > "../../../../../$LOG_FILE" 2>&1
+    )
     
     # Check if suite was successful
     if [ $? -eq 0 ]; then
