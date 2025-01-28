58940798bcffac116e78c430dd4ea2342086bae2
Ralf Haferkamp <r.haferkamp@opencloud.eu>
2025-01-28 09:42:52 +0100

    Rebrand default IDP clients

--- services/idp/pkg/config/defaults/defaultconfig.go
+++ services/idp/pkg/config/defaults/defaultconfig.go
@@ -86,9 +86,8 @@ func DefaultConfig() *config.Config {
 				},
 			},
 			{
-				ID:              "xdXOt13JKxym1B1QcEncf2XDkLAexMBFwiT9j6EfhhHFJhs2KM9jbjTmf8JBXE69",
-				Secret:          "UBntmLjC2yYCeHwsyj73Uwo9TAaecAetRwMw0xYcvNL9yRdLSUi0hUAHfvCHFeFh",
-				Name:            "ownCloud desktop app",
+				ID:              "OpenCloudDesktop",
+				Name:            "OpenCloud Desktop client",
 				ApplicationType: "native",
 				RedirectURIs: []string{
 					"http://127.0.0.1",
@@ -96,21 +95,19 @@ func DefaultConfig() *config.Config {
 				},
 			},
 			{
-				ID:              "e4rAsNUSIUs0lF4nbv9FmCeUkTlV9GdgTLDH1b5uie7syb90SzEVrbN7HIpmWJeD",
-				Secret:          "dInFYGV33xKzhbRmpqQltYNdfLdJIfJ9L5ISoKhNoT9qZftpdWSP71VrpGR9pmoD",
-				Name:            "ownCloud Android app",
+				ID:              "OpenCloudAndroid",
+				Name:            "OpenCloud Android app",
 				ApplicationType: "native",
 				RedirectURIs: []string{
-					"oc://android.owncloud.com",
+					"oc://android.opencloud.eu",
 				},
 			},
 			{
-				ID:              "mxd5OQDk6es5LzOzRvidJNfXLUZS2oN3oUFeXPP8LpPrhx3UroJFduGEYIBOxkY1",
-				Secret:          "KFeFWWEZO9TkisIQzR3fo7hfiMXlOpaqP8CFuTbSHzV1TUuGECglPxpiVKJfOXIx",
-				Name:            "ownCloud iOS app",
+				ID:              "OpenCloudIOS",
+				Name:            "OpenCloud iOS app",
 				ApplicationType: "native",
 				RedirectURIs: []string{
-					"oc://ios.owncloud.com",
+					"oc://ios.opencloud.com",
 				},
 			},
 		},
