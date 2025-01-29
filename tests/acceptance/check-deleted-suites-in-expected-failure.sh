116ff2e7b97583c3753b881d18eca7b2bdbc404b
Jörn Friedrich Dreyer <jfd@butonic.de>
2025-01-29 15:11:18 +0100

    update tests
    Signed-off-by: Jörn Friedrich Dreyer <jfd@butonic.de>

--- tests/acceptance/check-deleted-suites-in-expected-failure.sh
+++ tests/acceptance/check-deleted-suites-in-expected-failure.sh
@@ -18,7 +18,7 @@ log_success() {
 
 SCRIPT_PATH=$(dirname "$0")
 PATH_TO_SUITES="${SCRIPT_PATH}/features"
-EXPECTED_FAILURE_FILES=("expected-failures-localAPI-on-OCIS-storage.md" "expected-failures-API-on-OCIS-storage.md" "expected-failures-without-remotephp.md")
+EXPECTED_FAILURE_FILES=("expected-failures-localAPI-on-decomposed-storage.md" "expected-failures-API-on-decomposed-storage.md" "expected-failures-without-remotephp.md")
 # contains all the suites names inside tests/acceptance/features
 AVAILABLE_SUITES=($(ls -l "$PATH_TO_SUITES" | grep '^d' | awk '{print $NF}'))
 
