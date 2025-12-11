5742bca619a2494e2fbdf77c958fca875cd3bb96
Ralf Haferkamp <r.haferkamp@opencloud.eu>
2025-12-11 14:29:47 +0100

    fix(ci): Use relative path to allow running from different fork

--- tests/config/woodpecker/serve-hosting-discovery.sh
+++ tests/config/woodpecker/serve-hosting-discovery.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 while true; do
-  echo -e "HTTP/1.1 200 OK\n\n$(cat /woodpecker/src/github.com/opencloud-eu/opencloud/tests/config/woodpecker/hosting-discovery.xml)" | nc -l -k -p 8080
+  echo -e "HTTP/1.1 200 OK\n\n$(cat tests/config/woodpecker/hosting-discovery.xml)" | nc -l -k -p 8080
 done
