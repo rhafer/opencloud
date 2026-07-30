be75d8fe95f0738b578691a68e9bb55d668dd08b
André Duffeck <a.duffeck@opencloud.eu>
2026-07-30 15:25:13 +0200

    Reindex spaces concurrently
    This speeds up reindexing all spaces which happend sequentially space
    after space until now. The level of concurrency can be configured using
    the SEARCH_REINDEX_CONCURRENCY env var (3 by default).

--- services/search/pkg/config/defaults/defaultconfig.go
+++ services/search/pkg/config/defaults/defaultconfig.go
@@ -65,6 +65,7 @@ func DefaultConfig() *config.Config {
 		},
 		ContentExtractionSizeLimit: 20 * 1024 * 1024, // Limit content extraction to <20MB files by default
 		BatchSize:                  50,
+		ReindexConcurrency:         3,
 	}
 }
 
--- services/search/pkg/service/grpc/v0/service.go
+++ services/search/pkg/service/grpc/v0/service.go
@@ -19,6 +19,7 @@ import (
 	"github.com/opencloud-eu/reva/v2/pkg/utils"
 	merrors "go-micro.dev/v4/errors"
 	"go-micro.dev/v4/metadata"
+	"golang.org/x/sync/errgroup"
 	grpcmetadata "google.golang.org/grpc/metadata"
 
 	"github.com/opencloud-eu/opencloud/pkg/log"
@@ -144,13 +145,32 @@ func (s Service) IndexSpace(_ context.Context, in *searchsvc.IndexSpaceRequest, 
 		return errors.New(resp.GetStatus().GetMessage())
 	}
 
+	// Index all spaces concurrently, limited to a configurable number of spaces
+	// being reindexed at the same time.
+	concurrency := max(s.cfg.ReindexConcurrency, 1)
+
+	g, gctx := errgroup.WithContext(ctx)
+	g.SetLimit(concurrency)
+
 	for _, space := range resp.GetStorageSpaces() {
-		if err := s.searcher.IndexSpace(space.GetId(), in.GetForceReindex()); err != nil {
-			return err
+		// stop scheduling new work once one of the goroutines failed
+		if gctx.Err() != nil {
+			break
 		}
+
+		g.Go(func() error {
+			s.log.Info().Str("space_id", space.GetId().GetOpaqueId()).Msg("indexing space")
+			err := s.searcher.IndexSpace(space.GetId(), in.GetForceReindex())
+			if err != nil {
+				s.log.Error().Err(err).Str("space_id", space.GetId().GetOpaqueId()).Msg("failed to index space")
+			} else {
+				s.log.Info().Str("space_id", space.GetId().GetOpaqueId()).Msg("finished indexing space")
+			}
+			return err
+		})
 	}
 
-	return nil
+	return g.Wait()
 }
 
 // FromCache pulls a search result from cache
