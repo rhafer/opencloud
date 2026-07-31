673bf50c6b5acb2606ec378ca1fe39ba23377710
Andre Duffeck <aduffeck@users.noreply.github.com>
2026-07-31 16:39:48 +0200

    Merge pull request #3191 from pbleser-oc/feat/#3182-posixfs-scan-subtree
    feat(posixfs): #3182 add basepath option in the "posixfs scan" command
    
    (cherry picked from commit 942cca8eef991b4edf28725954138a00d0860968)

--- pkg/x/path/filepathx/path.go
+++ pkg/x/path/filepathx/path.go
@@ -1,7 +1,9 @@
 package filepathx
 
 import (
+	"fmt"
 	"path/filepath"
+	"strings"
 )
 
 // JailJoin joins any number of path elements into a single path,
@@ -9,4 +11,28 @@ import (
 // and ensuring that the path is always under the jail.
 func JailJoin(jail string, elem ...string) string {
 	return filepath.Join(jail, filepath.Join(append([]string{"/"}, elem...)...))
+}
+
+// Determines whether the file or directory 'child' is same as or underneath the directory 'parent'.
+//
+// Note that 'parent' is expected to be a directory.
+func IsSameOrContainedBy(parent string, child string) (bool, error) {
+	absParent, err := filepath.Abs(parent)
+	if err != nil {
+		return false, fmt.Errorf("failed to make parent directory absolute: %q: %w", parent, err)
+	}
+
+	absChild, err := filepath.Abs(child)
+	if err != nil {
+		return false, fmt.Errorf("failed to make child file/directory absolute: %q: %w", child, err)
+	}
+
+	rel, err := filepath.Rel(absParent, absChild)
+	if err != nil {
+		return false, fmt.Errorf("failed to determine the relative path between the parent directory %q and the child file/directory: %q: %w", absParent, absChild, err)
+	}
+	if rel == ".." || strings.HasPrefix(rel, ".."+string(filepath.Separator)) {
+		return false, nil
+	}
+	return true, nil
 }
