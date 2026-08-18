5d8d022bd73b26d5d74972d72d26aaf1d904ea7d
André Duffeck <a.duffeck@opencloud.eu>
2026-08-18 11:26:23 +0200

    Switch to Ginkgo tests for readability

--- services/search/pkg/opensearch/internal/test/helper.go
+++ services/search/pkg/opensearch/internal/test/helper.go
@@ -11,20 +11,20 @@ import (
 	"github.com/opencloud-eu/opencloud/pkg/conversions"
 )
 
-var TimeMustParse = func(t *testing.T, ts string) time.Time {
+var TimeMustParse = func(t testing.TB, ts string) time.Time {
 	tp, err := time.Parse(time.RFC3339Nano, ts)
 	require.NoError(t, err, "failed to parse time %s", ts)
 
 	return tp
 }
 
-func JSONMustMarshal(t *testing.T, data any) string {
+func JSONMustMarshal(t testing.TB, data any) string {
 	jsonData, err := json.Marshal(data)
 	require.NoError(t, err, "failed to marshal data to JSON")
 	return string(jsonData)
 }
 
-func SearchHitsMustBeConverted[T any](t *testing.T, hits []opensearchgoAPI.SearchHit) []T {
+func SearchHitsMustBeConverted[T any](t testing.TB, hits []opensearchgoAPI.SearchHit) []T {
 	ts := make([]T, len(hits))
 	for i, hit := range hits {
 		resource, err := conversions.To[T](hit.Source)
